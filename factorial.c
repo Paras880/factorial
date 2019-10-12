60d4533f7e1fbc5b2318897276916de67bdf74c3
Paras880 <56482932+Paras880@users.noreply.github.com>
2019-10-12 21:31:22 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -8,3 +8,14 @@ int main()
     for(i=2;i<n;i++)
         prod=prod*i;
     printf("factorial %d",prod);
+    //code in java
+    class FactorialExample{  
+     public static void main(String args[]){  
+      int i,fact=1;  
+      int number=5;//It is the number to calculate factorial    
+      for(i=1;i<=number;i++){    
+          fact=fact*i;    
+      }    
+      System.out.println("Factorial of "+number+" is: "+fact);    
+     }  
+    }  
